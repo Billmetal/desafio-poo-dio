f244b014af501b0831fdcd15e61666e5320ec77c
Willian K <billmetal9@gmail.com>
2023-04-03 13:13:30 -0300

    Criado README e estrutura inicial.

---
+++ src/br/com/homework/dio/bill/desafio/Main.java
@@ -0,0 +1,9 @@
+package br.com.homework.dio.bill.desafio;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/com/homework/dio/bill/desafio/dominio/Curso.java
@@ -0,0 +1,5 @@
+package br.com.homework.dio.bill.desafio.dominio;
+
+public class Curso {
+
+}
